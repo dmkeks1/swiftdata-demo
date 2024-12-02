773f49976aa67a630be8d883519f1cfba6d679e3
Dominik Zehe <zehedominik@googlemail.com>
2024-12-02 20:22:32 +0100

    - add button for delete, update, add

--- swiftdata-demo/ContentView.swift
+++ swiftdata-demo/ContentView.swift
@@ -12,12 +12,35 @@ struct ContentView: View {
     
     @Environment(\.modelContext) private var context // private because only used in this view
     
+    // Retrieve all of one data type from the db
+    @Query private var items: [DataItem] // updated autoamtically when we add data while app usage
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List(items) { item in
+                HStack {
+                    Text(item.creationDate.description)
+                    Spacer()
+                    // Update an item in the db
+                    Button("Update") {
+                        item.creationDate = Date()
+                    }
+                }
+                .swipeActions{
+                    Button("Delete") {
+                        // Delete an item from the db
+                        context.delete(item)
+                    }
+                }
+                
+            }
+            
+            Button("Add") {
+                // create a new data item
+                let data = DataItem()
+                // add it to database
+                context.insert(data)
+            }
         }
         .padding()
     }
