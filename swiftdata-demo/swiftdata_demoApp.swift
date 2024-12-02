eedeafe2a9d6f32010c795b1f2f165b79ad2a8de
Dominik Zehe <zehedominik@googlemail.com>
2024-12-02 17:51:04 +0100

    Initial Commit

---
+++ swiftdata-demo/swiftdata_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftdata_demoApp.swift
+//  swiftdata-demo
+//
+//  Created by Dominik Zehe on 02.12.24.
+//
+
+import SwiftUI
+
+@main
+struct swiftdata_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
